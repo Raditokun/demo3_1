d6304d5ec36b6df5544e7ec9f8a057c2fbcb46f7
Raditokun <radit040407@gmail.com>
2025-10-01 21:59:04 +0700

    bahasa gw

--- main.cpp
+++ main.cpp
@@ -4,22 +4,24 @@ using namespace std;
 
 int main()
 {
+    int j=0;//baris
     int n;
-    int j = 0;
-
-    cout << "masukkan suku ke n: ";
+    cout << "masukkan suku n: ";
     cin >> n;
 
-    do {
-        j = j + 1;
-        int i = 0;
-
-        do {
-            i = i + 1;
-            cout << i;
-        } while (i < j);
-        cout << endl;
-    } while (j < n);
+    do
+    {
+        j=j+1;
+        int i=0;
+        do
+        {
+            i++;
+            cout<<i;
+        }while(i<j);
+        cout<<endl;
+    }while(j<n);
 
     return 0;
+
+
 }
